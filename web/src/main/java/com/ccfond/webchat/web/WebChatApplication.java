c72d95078659d8ecb6d71130c0e322cfac07b76f
chenchao <chea0515@163.com>
2022-04-19 18:04:54 +0800

    java相关代码初始化

--- web/src/main/java/com/ccfond/webchat/web/WebChatApplication.java
+++ web/src/main/java/com/ccfond/webchat/web/WebChatApplication.java
@@ -1,19 +1,23 @@
-package com.ccfond.webchat.core;
+package com.ccfond.webchat.web;
 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
  * @author chea0515@163.com
  */
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
-public class ChatApplication {
+public class WebChatApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = SpringApplication.run(ChatApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(WebChatApplication.class, args);
         ConfigurableEnvironment env = ctx.getEnvironment();
         String serPort = env.getProperty("server.port");
         System.out.println("服务已启动，端口: " + serPort);
